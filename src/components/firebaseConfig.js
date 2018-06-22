13c24a05d5b8ead51e95efe1cb03408be02bf00f
vincegyan <vincegacayan@gmail.com>
2018-06-22 06:28:24 -0400

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCEJgX0xN2h24F6q2H-M2PT1gR0cTs3OHY",
+    authDomain: "vuefs-prod-85e19.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-85e19.firebaseio.com",
+    projectId: "vuefs-prod-85e19",
+    storageBucket: "vuefs-prod-85e19.appspot.com",
+    messagingSenderId: "1025989073678"
+}
